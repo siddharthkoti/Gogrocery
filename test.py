1d43ec7a06409aa690db8d63d23cf0c5746bcb1f
Siddharth R koti <siddharth.koti@gmail.com>
2017-11-23 21:17:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -16,13 +16,13 @@ import datetime
 
 # POSTGRES = {
     # 'user': 'okdddtuimwkwre',
-    # 'pw': '78ae3831414769bc68dc7b281f970db0d6e24b94edc2a40462a371800c2d6b2d',
+    # 'pw': ######,
     # 'db': 'd4g0u6alpfr93h',
     # 'host': 'ec2-107-22-160-199.compute-1.amazonaws.com',
     # 'port': '5432',
 # }
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://okdddtuimwkwre:78ae3831414769bc68dc7b281f970db0d6e24b94edc2a40462a371800c2d6b2d@ec2-107-22-160-199.compute-1.amazonaws.com:5432/d4g0u6alpfr93h' % POSTGRES
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://okdddtuimwkwre:######@ec2-107-22-160-199.compute-1.amazonaws.com:5432/d4g0u6alpfr93h' % POSTGRES
 # app.config['SECRET_KEY'] = 'super-secret'
 # app.config['SECURITY_REGISTERABLE'] = True
 
