e9e2f6c97cfbe94ccd7d5d2ec29435c092d7c3aa
Siddharth R koti <siddharth.koti@gmail.com>
2017-11-29 02:24:54 +0530

    deleted unwanted Files

--- database/insert.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO customer(cid, c_name,c_number, c_email, c_address,city,country)
-VALUES
- ( 'c1' , 'siddharth', 1122334455, 'siddharth@gmail.com','Dwarka Nagar', 'Bangalore', 'India');
-INSERT INTO customer(cid, c_name,c_number, c_email, c_address,city,country)
-VALUES
- ( 'c2' , 'shashi', 6572987276, 'shashi@gmail.com','Dwarka Nagar', 'Bangalore', 'India');
- 
