7e30d564f432b4304ddc3ee6fe2bb492b420f903
shriharipg05 <shriharipg05@gmail.com>
2017-11-29 09:50:50 +0530

    updated templates

--- settings.py
+++ settings.py
@@ -6,13 +6,13 @@ db = SQLAlchemy(app)
 
 POSTGRES = {
     'user': 'okdddtuimwkwre',
-    'pw': '################',
+    'pw': '78ae3831414769bc68dc7b281f970db0d6e24b94edc2a40462a371800c2d6b2d',
     'db': 'd4g0u6alpfr93h',
     'host': 'ec2-107-22-160-199.compute-1.amazonaws.com',
     'port': '5432',
 }
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://okdddtuimwkwre:#####################@ec2-107-22-160-199.compute-1.amazonaws.com:5432/d4g0u6alpfr93h' % POSTGRES
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://okdddtuimwkwre:78ae3831414769bc68dc7b281f970db0d6e24b94edc2a40462a371800c2d6b2d@ec2-107-22-160-199.compute-1.amazonaws.com:5432/d4g0u6alpfr93h' % POSTGRES
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['SECURITY_REGISTERABLE'] = True
